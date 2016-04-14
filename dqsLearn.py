9184384e97effe7b787892a49c9af9cd176ec04f
st33l-butterfly <dominic.routley@gmail.com>
2016-04-15 00:44:10 +0100

    Test background and filler

--- dqsLearn.py
+++ dqsLearn.py
@@ -314,7 +314,7 @@ class lesson_2(Frame):
 
 class test_1(Frame):    #Dom Routley
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
+        Frame.__init__(self, parent, bg=BACKGROUND_COLOUR_DARKER)
 
         self.tstart = datetime.now()  # get current time
 
@@ -341,13 +341,13 @@ class test_1(Frame):    #Dom Routley
         label.grid(row=1, column=0)
 
         Q1R = Radiobutton(self, text="A correct proposition.", variable=self.varQ1A, value="x1")
-        Q1R.grid(row=1, column=1)
+        Q1R.grid(row=1, column=1, sticky=N+E+S+W)
         Q1R = Radiobutton(self, text="A incorrect proposition.", variable=self.varQ1A, value="y1")
-        Q1R.grid(row=1, column=2)
+        Q1R.grid(row=1, column=2, sticky=N+E+S+W)
         Q1R = Radiobutton(self, text="A always correct proposition.", variable=self.varQ1A, value="t1")
-        Q1R.grid(row=2, column=1)
+        Q1R.grid(row=2, column=1, sticky=N+E+S+W)
         Q1R = Radiobutton(self, text="A always incorrect proposition.", variable=self.varQ1A, value="z1")
-        Q1R.grid(row=2, column=2)
+        Q1R.grid(row=2, column=2, sticky=N+E+S+W)
 
         sep = ttk.Separator(self, orient=HORIZONTAL)
         sep.grid(row=3, columnspan=4, sticky=EW)
@@ -356,13 +356,13 @@ class test_1(Frame):    #Dom Routley
         label.grid(row=4, column=0)
 
         Q2R = Radiobutton(self, text="¬qV¬p => ¬(p^q)", variable=self.varQ2A, value="t2")
-        Q2R.grid(row=4, column=1)
+        Q2R.grid(row=4, column=1, sticky=N+E+S+W)
         Q2R = Radiobutton(self, text="p => ¬q", variable=self.varQ2A, value="y2")
-        Q2R.grid(row=4, column=2)
+        Q2R.grid(row=4, column=2, sticky=N+E+S+W)
         Q2R = Radiobutton(self, text="p^q = qVp", variable=self.varQ2A, value="x2")
-        Q2R.grid(row=5, column=1)
+        Q2R.grid(row=5, column=1, sticky=N+E+S+W)
         Q2R = Radiobutton(self, text="q = ¬(¬q)", variable=self.varQ2A, value="z2")
-        Q2R.grid(row=5, column=2)
+        Q2R.grid(row=5, column=2, sticky=N+E+S+W)
 
         sep = ttk.Separator(self, orient=HORIZONTAL)
         sep.grid(row=6, columnspan=4, sticky=EW)
@@ -371,13 +371,13 @@ class test_1(Frame):    #Dom Routley
         label.grid(row=7, column=0)
 
         Q3R = Radiobutton(self, text="q^¬p => ¬(q^p)", variable=self.varQ3A, value="x3")
-        Q3R.grid(row=7, column=1)
+        Q3R.grid(row=7, column=1, sticky=N+E+S+W)
         Q3R = Radiobutton(self, text="q => ¬q^¬q", variable=self.varQ3A, value="y3")
-        Q3R.grid(row=7, column=2)
+        Q3R.grid(row=7, column=2, sticky=N+E+S+W)
         Q3R = Radiobutton(self, text="q => p", variable=self.varQ3A, value="z3")
-        Q3R.grid(row=8, column=1)
+        Q3R.grid(row=8, column=1, sticky=N+E+S+W)
         Q3R = Radiobutton(self, text="¬pV¬q => ¬(p^q)", variable=self.varQ3A, value="t3")
-        Q3R.grid(row=8, column=2)
+        Q3R.grid(row=8, column=2, sticky=N+E+S+W)
 
         sep = ttk.Separator(self, orient=HORIZONTAL)
         sep.grid(row=9, columnspan=4, sticky=EW)
@@ -386,13 +386,13 @@ class test_1(Frame):    #Dom Routley
         label.grid(row=10, column=0)
 
         Q4C = Checkbutton(self, text="x+y-(f(j/k*67.462)) > 1067", variable=self.varQ4T1)
-        Q4C.grid(row=10, column=1)
+        Q4C.grid(row=10, column=1, sticky=N+E+S+W)
         Q4C = Checkbutton(self, text="The sky is blue", variable=self.varQ4T2)
-        Q4C.grid(row=10, column=2)
+        Q4C.grid(row=10, column=2, sticky=N+E+S+W)
         Q4C = Checkbutton(self, text="Star Wars or Star Trek", variable=self.varQ4F1)
-        Q4C.grid(row=11, column=1)
+        Q4C.grid(row=11, column=1, sticky=N+E+S+W)
         Q4C = Checkbutton(self, text="Can I have a drink", variable=self.varQ4F2)
-        Q4C.grid(row=11, column=2)
+        Q4C.grid(row=11, column=2, sticky=N+E+S+W)
 
         sep = ttk.Separator(self, orient=HORIZONTAL)
         sep.grid(row=12, columnspan=4, sticky=EW)
@@ -402,40 +402,40 @@ class test_1(Frame):    #Dom Routley
 
         # truth table, header
         Q5L = Label(self, text="P")
-        Q5L.grid(row=13, column=1)
+        Q5L.grid(row=13, column=1, sticky=N+E+S+W)
         Q5L = Label(self, text="Q")
-        Q5L.grid(row=13, column=2)
+        Q5L.grid(row=13, column=2, sticky=N+E+S+W)
         Q5L = Label(self, text="AND")
-        Q5L.grid(row=13, column=3)
+        Q5L.grid(row=13, column=3, sticky=N+E+S+W)
 
         # truth table, data
         Q5Tr1 = Label(self, text="T")
-        Q5Tr1.grid(row=14, column=1)
+        Q5Tr1.grid(row=14, column=1, sticky=N+E+S+W)
         Q5Tr2 = Label(self, text="T")
-        Q5Tr2.grid(row=15, column=1)
+        Q5Tr2.grid(row=15, column=1, sticky=N+E+S+W)
         Q5Tr3 = Label(self, text="T")
-        Q5Tr3.grid(row=14, column=2)
+        Q5Tr3.grid(row=14, column=2, sticky=N+E+S+W)
         Q5Tr4 = Label(self, text="T")
-        Q5Tr4.grid(row=16, column=2)
+        Q5Tr4.grid(row=16, column=2, sticky=N+E+S+W)
 
         Q5F1 = Label(self, text="F")
-        Q5F1.grid(row=15, column=2)
+        Q5F1.grid(row=15, column=2, sticky=N+E+S+W)
         Q5F2 = Label(self, text="F")
-        Q5F2.grid(row=16, column=1)
+        Q5F2.grid(row=16, column=1, sticky=N+E+S+W)
         Q5F3 = Label(self, text="F")
-        Q5F3.grid(row=17, column=1)
+        Q5F3.grid(row=17, column=1, sticky=N+E+S+W)
         Q5F4 = Label(self, text="F")
-        Q5F4.grid(row=17, column=2)
+        Q5F4.grid(row=17, column=2, sticky=N+E+S+W)
 
         # truth table input
         Q5I = Entry(self, width=1, textvariable=self.varQ5T)
-        Q5I.grid(row=14, column=3)
+        Q5I.grid(row=14, column=3, sticky=N+E+S+W)
         Q5I = Entry(self, width=1, textvariable=self.varQ5F1)
-        Q5I.grid(row=15, column=3)
+        Q5I.grid(row=15, column=3, sticky=N+E+S+W)
         Q5I = Entry(self, width=1, textvariable=self.varQ5F2)
-        Q5I.grid(row=16, column=3)
+        Q5I.grid(row=16, column=3, sticky=N+E+S+W)
         Q5I = Entry(self, width=1, textvariable=self.varQ5F3)
-        Q5I.grid(row=17, column=3)
+        Q5I.grid(row=17, column=3, sticky=N+E+S+W)
 
 
 
@@ -521,7 +521,7 @@ class lecturerMenu(Frame):
 
 class view_results(Frame):  #Dom Routley
     def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
+        Frame.__init__(self, parent, bg=BACKGROUND_COLOUR_DARKER)
 
         #PLACEHOLDER DATA
         userIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
