88fe7883ebf7edb4f3c2b5b6807cb5beabb7dc63
Oliver <o.copleston@gmail.com>
2016-04-15 06:03:14 +0100

    username is now global variable

--- dqsLearn.py
+++ dqsLearn.py
@@ -82,6 +82,7 @@ class login(Frame):
 
     def _login_btn_clicked(self):
         # print("Clicked")
+        global username
         username = self.entry_1.get()
         password = self.entry_2.get()
 
@@ -97,16 +98,12 @@ class login(Frame):
             if (student_usernames.index(username) == student_passwords.index(password)):
                 tm.showinfo("Login info", "Welcome Student")
                 self.controller.show_frame(studentMenu)
-                self.line1.destroy()
-                self.line2.destroy()
             else:
                 tm.showerror("Login error", "This user does not exist, perhaps you've entered the wrong user ID")
         elif username in teacher_usernames and password in teacher_passwords:
             if (teacher_usernames.index(username) == teacher_passwords.index(password)):
                 tm.showinfo("Login info", "Welcome Teacher")
                 self.controller.show_frame(lecturerMenu)
-                self.line1.destroy()
-                self.line2.destroy()
             else:
                 tm.showerror("Login error", "This user does not exist, perhaps you've entered the wrong user ID")
         else:
@@ -678,15 +675,18 @@ class test_2(Frame):    #Dom Routley
             tfinish = datetime.now()
             timeElapsed = tfinish - self.tstart2
 
-            newResult = dqsClass.UserResult("Sets", "0001", timeElapsed, questions)
+            newResult = dqsClass.UserResult("Sets", username, timeElapsed, questions)
 
-            print(newResult.lessonID)
-            print(newResult.userID)
+            # testing object
+            print(newResult.testID)
+            print(newResult.studentID)
             print(newResult.timeElapsed)
             print(newResult.questions)
 
-            db = shelve.open("responses2.dat", "n")
-            db[newResult.lessonID] = newResult
+            # creating object
+            db = shelve.open("responses2.dat", 'n')
+            # storing object in Pickle db
+            db[username] = newResult
             db.close()
 
             controller.show_frame(studentMenu)
